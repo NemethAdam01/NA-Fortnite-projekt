ee5f9cc19ad6a8c0559deb14784a1d126aa2a290
NemethAdam01 <nemeth.adam@diak.szbi-pg.hu>
2025-12-01 09:37:34 +0100

    create players

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,410 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Level = new System.Windows.Forms.Label();
+            this.lbl_HP = new System.Windows.Forms.Label();
+            this.lbl_Shield = new System.Windows.Forms.Label();
+            this.pb_item_1 = new System.Windows.Forms.PictureBox();
+            this.pb_item_2 = new System.Windows.Forms.PictureBox();
+            this.pb_item_3 = new System.Windows.Forms.PictureBox();
+            this.pb_item_4 = new System.Windows.Forms.PictureBox();
+            this.pb_item_5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_ammo_1 = new System.Windows.Forms.Label();
+            this.lbl_ammo_4 = new System.Windows.Forms.Label();
+            this.lbl_ammo_3 = new System.Windows.Forms.Label();
+            this.lbl_ammo_2 = new System.Windows.Forms.Label();
+            this.lbl_ammo_5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(262, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(345, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 66);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(428, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 66);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(511, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 66);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(594, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(77, 66);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(677, 12);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(77, 66);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Stats:";
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(31, 117);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(41, 13);
+            this.lbl_Name.TabIndex = 9;
+            this.lbl_Name.Text = "Name: ";
+            // 
+            // lbl_Level
+            // 
+            this.lbl_Level.AutoSize = true;
+            this.lbl_Level.Location = new System.Drawing.Point(31, 148);
+            this.lbl_Level.Name = "lbl_Level";
+            this.lbl_Level.Size = new System.Drawing.Size(39, 13);
+            this.lbl_Level.TabIndex = 10;
+            this.lbl_Level.Text = "Level: ";
+            // 
+            // lbl_HP
+            // 
+            this.lbl_HP.AutoSize = true;
+            this.lbl_HP.Location = new System.Drawing.Point(31, 176);
+            this.lbl_HP.Name = "lbl_HP";
+            this.lbl_HP.Size = new System.Drawing.Size(25, 13);
+            this.lbl_HP.TabIndex = 11;
+            this.lbl_HP.Text = "HP:";
+            // 
+            // lbl_Shield
+            // 
+            this.lbl_Shield.AutoSize = true;
+            this.lbl_Shield.Location = new System.Drawing.Point(31, 199);
+            this.lbl_Shield.Name = "lbl_Shield";
+            this.lbl_Shield.Size = new System.Drawing.Size(39, 13);
+            this.lbl_Shield.TabIndex = 12;
+            this.lbl_Shield.Text = "Shield:";
+            // 
+            // pb_item_1
+            // 
+            this.pb_item_1.Location = new System.Drawing.Point(13, 276);
+            this.pb_item_1.Name = "pb_item_1";
+            this.pb_item_1.Size = new System.Drawing.Size(160, 160);
+            this.pb_item_1.TabIndex = 13;
+            this.pb_item_1.TabStop = false;
+            // 
+            // pb_item_2
+            // 
+            this.pb_item_2.Location = new System.Drawing.Point(179, 276);
+            this.pb_item_2.Name = "pb_item_2";
+            this.pb_item_2.Size = new System.Drawing.Size(160, 160);
+            this.pb_item_2.TabIndex = 14;
+            this.pb_item_2.TabStop = false;
+            // 
+            // pb_item_3
+            // 
+            this.pb_item_3.Location = new System.Drawing.Point(345, 278);
+            this.pb_item_3.Name = "pb_item_3";
+            this.pb_item_3.Size = new System.Drawing.Size(160, 160);
+            this.pb_item_3.TabIndex = 15;
+            this.pb_item_3.TabStop = false;
+            // 
+            // pb_item_4
+            // 
+            this.pb_item_4.Location = new System.Drawing.Point(511, 278);
+            this.pb_item_4.Name = "pb_item_4";
+            this.pb_item_4.Size = new System.Drawing.Size(160, 160);
+            this.pb_item_4.TabIndex = 16;
+            this.pb_item_4.TabStop = false;
+            // 
+            // pb_item_5
+            // 
+            this.pb_item_5.Location = new System.Drawing.Point(677, 276);
+            this.pb_item_5.Name = "pb_item_5";
+            this.pb_item_5.Size = new System.Drawing.Size(160, 160);
+            this.pb_item_5.TabIndex = 17;
+            this.pb_item_5.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Items:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(235, 110);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(164, 128);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(523, 110);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(314, 128);
+            this.richTextBox2.TabIndex = 20;
+            this.richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(651, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Leaderboard:";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(442, 137);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 22;
+            this.button9.Text = "Kills";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(442, 166);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 23;
+            this.button10.Text = "HP";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(442, 194);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 24;
+            this.button11.Text = "XP";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(460, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Order:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Ammo:";
+            // 
+            // lbl_ammo_1
+            // 
+            this.lbl_ammo_1.AutoSize = true;
+            this.lbl_ammo_1.Location = new System.Drawing.Point(80, 249);
+            this.lbl_ammo_1.Name = "lbl_ammo_1";
+            this.lbl_ammo_1.Size = new System.Drawing.Size(13, 13);
+            this.lbl_ammo_1.TabIndex = 27;
+            this.lbl_ammo_1.Text = "1";
+            // 
+            // lbl_ammo_4
+            // 
+            this.lbl_ammo_4.AutoSize = true;
+            this.lbl_ammo_4.Location = new System.Drawing.Point(578, 249);
+            this.lbl_ammo_4.Name = "lbl_ammo_4";
+            this.lbl_ammo_4.Size = new System.Drawing.Size(13, 13);
+            this.lbl_ammo_4.TabIndex = 28;
+            this.lbl_ammo_4.Text = "1";
+            // 
+            // lbl_ammo_3
+            // 
+            this.lbl_ammo_3.AutoSize = true;
+            this.lbl_ammo_3.Location = new System.Drawing.Point(408, 249);
+            this.lbl_ammo_3.Name = "lbl_ammo_3";
+            this.lbl_ammo_3.Size = new System.Drawing.Size(13, 13);
+            this.lbl_ammo_3.TabIndex = 29;
+            this.lbl_ammo_3.Text = "1";
+            // 
+            // lbl_ammo_2
+            // 
+            this.lbl_ammo_2.AutoSize = true;
+            this.lbl_ammo_2.Location = new System.Drawing.Point(246, 249);
+            this.lbl_ammo_2.Name = "lbl_ammo_2";
+            this.lbl_ammo_2.Size = new System.Drawing.Size(13, 13);
+            this.lbl_ammo_2.TabIndex = 30;
+            this.lbl_ammo_2.Text = "1";
+            // 
+            // lbl_ammo_5
+            // 
+            this.lbl_ammo_5.AutoSize = true;
+            this.lbl_ammo_5.Location = new System.Drawing.Point(741, 249);
+            this.lbl_ammo_5.Name = "lbl_ammo_5";
+            this.lbl_ammo_5.Size = new System.Drawing.Size(13, 13);
+            this.lbl_ammo_5.TabIndex = 31;
+            this.lbl_ammo_5.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(851, 450);
+            this.Controls.Add(this.lbl_ammo_5);
+            this.Controls.Add(this.lbl_ammo_2);
+            this.Controls.Add(this.lbl_ammo_3);
+            this.Controls.Add(this.lbl_ammo_4);
+            this.Controls.Add(this.lbl_ammo_1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pb_item_5);
+            this.Controls.Add(this.pb_item_4);
+            this.Controls.Add(this.pb_item_3);
+            this.Controls.Add(this.pb_item_2);
+            this.Controls.Add(this.pb_item_1);
+            this.Controls.Add(this.lbl_Shield);
+            this.Controls.Add(this.lbl_HP);
+            this.Controls.Add(this.lbl_Level);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "NÁ-FortniteProject";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_item_5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Level;
+        private System.Windows.Forms.Label lbl_HP;
+        private System.Windows.Forms.Label lbl_Shield;
+        private System.Windows.Forms.PictureBox pb_item_1;
+        private System.Windows.Forms.PictureBox pb_item_2;
+        private System.Windows.Forms.PictureBox pb_item_3;
+        private System.Windows.Forms.PictureBox pb_item_4;
+        private System.Windows.Forms.PictureBox pb_item_5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_ammo_1;
+        private System.Windows.Forms.Label lbl_ammo_4;
+        private System.Windows.Forms.Label lbl_ammo_3;
+        private System.Windows.Forms.Label lbl_ammo_2;
+        private System.Windows.Forms.Label lbl_ammo_5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace NA_Fortnite_projekt
 {
     public partial class Form1 : Form
     {
+        public static Random rnd = new Random();
+        public static List<Player> players = new List<Player>();
+        public static List<Weapon> weapons = new List<Weapon>();
+        public static List<string> cosmetics = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string namesBe = File.ReadAllText("names.txt",Encoding.UTF8);
+            string weaponBe  = File.ReadAllText("weapons.txt", Encoding.UTF8);
+            string cosmeticsBe = File.ReadAllText("items.txt", Encoding.UTF8);
+            List<string> nevek = new List<string>();
+            foreach (var item in namesBe.Split(','))
+            {
+                nevek.Add(item);
+            }
+            foreach (var item in cosmeticsBe.Split(','))
+            {
+                cosmetics.Add(item);
+            }
+            foreach (var item in weaponBe.Split(';'))
+            {
+                weapons.Add(new Weapon(item));
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                players.Add(new Player(nevek[rnd.Next(0, nevek.Count)]));
+                for (int j = 0; j < 3; j++)
+                {
+                    players[i].cosmetics.Add(cosmetics[rnd.Next(0,cosmetics.Count)]);
+                }
+                for (int k = 0; k < 5; k++)
+                {
+                    players[i].weapons.Add(weapons[rnd.Next(0,weapons.Count)]);
+                }
+            }
+            
+            
+
+
+        }
+    }
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int HP { get; set; }
+        public int Shield { get; set; }
+        public List<string> cosmetics = new List<string>();
+        public List<Weapon> weapons = new List<Weapon>();
+        public Player(string nev)
+        {
+            Random rnd = new Random();
+            Name = nev;
+            Level = rnd.Next(1, 201);
+            HP = rnd.Next(1, 101);
+            Shield = rnd.Next(1, 101);
+        }
+    }
+    public class Weapon
+    {
+        public string Name { get; set; }
+        public int MagazineSize { get; set; }
+        public int CurrentAmmo { get; set; }
+        public Weapon(string sor)
+        {
+            Random rnd = new Random();
+            Name = sor.Split(',')[0];
+            MagazineSize = int.Parse(sor.Split(',')[1]);
+            CurrentAmmo = rnd.Next(0, MagazineSize + 1);
         }
     }
 }
